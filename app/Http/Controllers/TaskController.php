3b5ac36348b1e374f9899b8e91b7d7fc8ead46e2
Madan Neupane <nmadan692@gmail.com>
2023-08-07 16:55:55 +0900

    initial commit

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Task;
+
+class TaskController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth:api');
+    }
+
+    public function index()
+    {
+        $tasks = Task::all();
+        return response()->json([
+            'status' => 'success',
+            'tasks' => $tasks,
+        ]);
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string',
+            'start_date' => 'required|date_format:Y-m-d H:i:s',
+            'end_date' => 'required|date_format:Y-m-d H:i:s',
+            'priority' => 'required|string',
+            'remarks' => 'string',
+        ]);
+
+        $task = Task::create([
+            'name' => $request->name,
+            'start_date' => $request->start_date,
+            'end_date' => $request->end_date,
+            'priority' => $request->priority,
+            'remarks' => $request->remarks,
+        ]);
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'task created successfully',
+            'task' => $task,
+        ]);
+    }
+
+    public function show($id)
+    {
+        $task = Task::find($id);
+        return response()->json([
+            'status' => 'success',
+            'task' => $task,
+        ]);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'name' => 'required|string',
+            'start_date' => 'required|date_format:Y-m-d H:i:s',
+            'end_date' => 'required|date_format:Y-m-d H:i:s',
+            'priority' => 'required|string',
+            'is_deleted' => 'boolean',
+            'remarks' => 'string',
+        ]);
+
+        $task = Task::find($id);
+        $task->name = $request->name;
+        $task->start_date = $request->start_date;
+        $task->end_date = $request->end_date;
+        $task->priority = $request->priority;
+        $task->is_deleted = $request->is_deleted;
+        $task->remarks = $request->remarks;
+        $task->save();
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'task updated successfully',
+            'task' => $task,
+        ]);
+    }
+
+    public function destroy($id)
+    {
+        $task = Task::find($id);
+        $task->delete();
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'task deleted successfully',
+            'task' => $task,
+        ]);
+    }
+}
---
+++ database/migrations/2023_08_07_072728_create_tasks_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->timestamp('start_date')->nullable(false); // Not nullable
+            $table->timestamp('end_date')->nullable(false);   // Not nullable
+            $table->boolean('is_deleted')->default(false);    // Default value is false
+            $table->string('remarks');
+            $table->string('priority')->nullable();           // Nullable
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
