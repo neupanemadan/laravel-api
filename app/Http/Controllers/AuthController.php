4298b8d5083d97599b08cd80dbf3a6fe81e65f6a
Madan Neupane <nmadan692@gmail.com>
2023-08-07 17:32:39 +0900

    fix: jwt auth

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -34,10 +34,7 @@ class AuthController extends Controller
         return response()->json([
                 'status' => 'success',
                 'user' => $user,
-                'authorisation' => [
-                    'token' => $token,
-                    'type' => 'bearer',
-                ]
+                'access_token' =>  $token
             ]);
 
     }
